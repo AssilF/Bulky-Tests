2b525ab4feae54aeddda3b27ed2c564dfdc1dee3
AssilF <43552271+AssilF@users.noreply.github.com>
2025-09-20 22:58:34 +0100

    Respond to ILITE broadcasts

--- include/EspNowDiscovery.h
+++ include/EspNowDiscovery.h
@@ -34,18 +34,18 @@ class EspNowDiscovery {
   void handleIncoming(const uint8_t *mac, const uint8_t *data, int len);
   void handleDiscoveryPacket(const uint8_t *mac, const Comm::DiscoveryPacket &packet);
   void handleFeedbackPacket(const Comm::FeedbackPacket &packet);
-  void sendScanRequest();
   void sendIliteIdentity(const std::array<uint8_t, 6> &mac, const String &droneName);
+  void sendIliteDroneIdentity(const std::array<uint8_t, 6> &mac);
+  void sendIliteDroneAck(const std::array<uint8_t, 6> &mac);
 
   bool ensurePeer(const std::array<uint8_t, 6> &mac);
   bool handleIliteMessage(const uint8_t *mac, const uint8_t *data, int len);
 
   Comm::PeerRegistry &registry_;
-  std::array<uint8_t, 6> controllerMac_{};
+  std::array<uint8_t, 6> localMac_{};
   std::array<uint8_t, 6> targetMac_{};
   bool hasTarget_ = false;
   bool scanning_ = false;
-  uint32_t lastScanBroadcastMs_ = 0;
   TaskHandle_t taskHandle_ = nullptr;
   mutable portMUX_TYPE stateMutex_ = portMUX_INITIALIZER_UNLOCKED;
 };
